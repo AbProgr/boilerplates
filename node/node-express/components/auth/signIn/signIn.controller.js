59343d02942d6473759d8d038a3c3be94f3980d5
Kumar Abhijeet <ab.progr@gmail.com>
2020-05-05 00:09:42 +0530

    fixed error handler in signin controller

--- node/node-express/components/auth/signIn/signIn.controller.js
+++ node/node-express/components/auth/signIn/signIn.controller.js
@@ -3,10 +3,12 @@ const { signInHandler } = require("./signIn.model");
 const signInController = async (req, res) => {
   const { email, password } = req.body;
   if (email && password) {
-    const data = await signInHandler(email, password).catch((err) => {
-      res.status(403).json({ status: "error", error: err, data: null });
-    });
-    res.status(200).json({ status: "success", statusCode: 200, data });
+    try {
+      const data = await signInHandler(email, password);
+      res.status(200).json({ status: "success", statusCode: 200, data });
+    } catch ({ message }) {
+      res.status(403).json({ status: "error", error: message, data: null });
+    }
   }
 };
 
